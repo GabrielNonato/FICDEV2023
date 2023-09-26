c987d08c4fa01508fee8b5c8ede5d90ff8dea1f4
GabrielNonato <gabrielsnonato1@gmail.com>
2023-09-26 17:34:55 -0300

    Login e Cadastro 99Porcento

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Form, Button, Col, Container } from "react-bootstrap";
+import { Form, Button} from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 
+import '../style.css'
+
 import { Input } from "../components/Input";
-import { Header } from '../components/Header';
 import { Modal } from '../components/Modal';
 
 import { loginUsuario } from '../services/usuario-services';
@@ -28,57 +29,79 @@ export function Login() {
     }
 
     return (
-        <Container>
+        <div className="container d-flex justify-content-center align-items-center min-vh-100">
             <Modal
                 show={result}
                 title={result?.title}
                 message={result?.message}
                 handleClose={() => setResult(null)}
             />
-            <Header title="Entre na sua conta" />
-            <Form
-                noValidate
-                validated={!errors}
-                onSubmit={handleSubmit(onSubmit)}
-                autoComplete='off'
-                className="bg-light rounded p-5 shadow w-50 m-auto"
-            >
-                <Col>
-                    <Input
-                        className="mb-4"
-                        controlId="formGroupEmail"
-                        label="E-mail"
-                        type="email"
-                        name="email"
-                        errors={errors.email}
-                        placeholder="Insira seu e-mail"
-                        validations={register('email', {
-                            required: {
-                                value: false,
-                            },
-                            
-                        })}
-                    />
-                    <Input
-                        className="mb-4"
-                        controlId="formGroupSenha"
-                        label="Senha"
-                        type="password"
-                        name="senha"
-                        errors={errors.senha}
-                        placeholder="Insira sua senha"
-                        validations={register('senha', {
-                            required: {
-                                value: false,
-                            }
-                        })}
-                    />
-                    <div className="d-flex justify-content-between">
-                        <Button type="submit" disabled={!isValid}>Entrar</Button>
-                        <Link to="/register">Criar conta</Link>
+        
+           <div className="row border rounded-5 p-3 bg-white shadow box-area">
+        
+            <div className="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box bg-dark">
+                <div className="featured-image mb-3">
+                    
+                </div>
+                <p className="text-light">Sobre nós:</p>
+                <small className="text-light">Somos um sistema de gestão de salas. Você encontrará diversas funcionalidades para reserva e criação de salas além de um dashboard completo.</small>
+            </div> 
+       
+           <div className="col-md-6 right-box">
+              <div className="row align-items-center">
+                    <div className="header-text mb-4">
+                         <h2 className="text-center">Bem vindo!</h2>
+                         <p className="text-center">Por favor, conecte-se na sua conta.</p>
                     </div>
-                </Col>
-            </Form>
-        </Container>
-    );
+
+                    <Form
+                            noValidate
+                            validated={!errors}
+                            onSubmit={handleSubmit(onSubmit)}
+                            autoComplete='off'
+                            className="form-control form-control-lg bg-light fs-6"
+                        >
+                        
+                            <Input
+                                className="mb-4"
+                                controlId="formGroupEmail"
+                                label="E-mail"
+                                type="email"
+                                name="email"
+                                errors={errors.email}
+                                placeholder="Insira seu e-mail"
+                                validations={register('email', {
+                                    required: {
+                                        value: false,
+                                    },
+
+                                })}
+                            />
+                            <Input
+                                className="mb-4"
+                                controlId="formGroupSenha"
+                                label="Senha"
+                                type="password"
+                                name="senha"
+                                errors={errors.senha}
+                                placeholder="Insira sua senha"
+                                validations={register('senha', {
+                                    required: {
+                                        value: false,
+                                    }
+                                })}
+                            />
+                            <Button type="submit" className="btn btn-dark btn-lg w-100 fs-6" disabled={!isValid}>Entrar</Button>
+                    </Form>
+
+
+                    <div className="row">
+                        <small className="text-center">Não possue conta? <Link to="/register" className="text-decoration-none text-dark"><strong>Cadastre-se</strong></Link></small>
+                    </div>
+              </div>
+           </div> 
+          </div>
+        </div>
+    
+  );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { Button, Col, Container, Form } from "react-bootstrap";
+import { Button, Col,  Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 
 import { Input } from "../components/Input";
-import { Header } from '../components/Header';
+
 import { Modal } from '../components/Modal';
 
 import { signupUsuario } from "../services/usuario-services";
@@ -26,8 +26,112 @@ export function Register() {
             });
         }
     }
-
     return (
+        <div className="container d-flex justify-content-center align-items-center min-vh-100">
+            <Modal
+                show={result}
+                title={result?.title}
+                message={result?.message}
+                handleClose={() => setResult(null)}
+            />
+        
+           <div className="row border rounded-5 p-3 bg-white shadow box-area">
+        
+            <div className="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box bg-dark">
+                <div className="featured-image mb-3">
+                    
+                </div>
+                <p className="text-light">Sobre nós:</p>
+                <small className="text-light">Somos um sistema de gestão de salas. Você encontrará diversas funcionalidades para reserva e criação de salas além de um dashboard completo.</small>
+            </div> 
+       
+           <div className="col-md-6 right-box">
+              <div className="row align-items-center">
+                    <div className="header-text mb-4">
+                         <h2 className="text-center">Bem vindo!</h2>
+                         <p className="text-center">Por favor, preencha os campos a seguir para concluirmos seu cadastro.</p>
+                    </div>
+
+                    <Form
+                noValidate
+                validated={!errors}
+                onSubmit={handleSubmit(onSubmit)}
+                autoComplete='off'
+                className="form-control form-control-lg bg-light fs-6"
+            >
+                <Col>
+                    <Input
+                        className="mb-4"
+                        controlId="formGroupEmail"
+                        label="E-mail"
+                        type="email"
+                        name="email"
+                        errors={errors.email}
+                        placeholder="Insira seu e-mail"
+                        validations={register('email', {
+                            required: {
+                                value: true,
+                                message: 'E-mail é obrigatório'
+                            },
+                            pattern: {
+                                value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                                message: 'E-mail inválido!'
+                            }
+                        })}
+                    />
+                    <Input
+                        className="mb-4"
+                        controlId="formGroupSenha"
+                        label="Senha"
+                        type="password"
+                        name="senha"
+                        errors={errors.senha}
+                        placeholder="Insira sua senha"
+                        validations={register('senha', {
+                            required: {
+                                value: true,
+                                message: 'Senha é obrigatória'
+                            },
+                            minLength:{
+                                value:6,
+                                message: 'Minimo 6 caracteres'
+                            },
+                        })}
+                    />
+                    <Input
+                        className="mb-4"
+                        controlId="formGroupNome"
+                        label="Nome"
+                        type="text"
+                        name="nome"
+                        errors={errors.nome}
+                        placeholder="Insira seu nome"
+                        validations={register('nome', {
+                            required: {
+                                value: true,
+                                message: 'Nome é obrigatório'
+                            }
+                        })}
+                    />
+                    <div className="d-flex justify-content-between">
+                        <Button className="btn btn-dark btn-lg w-100 fs-6" type="submit" disabled={!isValid}>Criar</Button>
+                    </div>
+                </Col>
+            </Form>
+            <Link className="text-center text-decoration-none text-dark" to="/"><strong>Já tenho uma conta</strong></Link>
+
+                    
+              </div>
+           </div> 
+          </div>
+        </div>
+    
+  );
+    
+}
+
+/*
+return (
         <Container>
             <Modal
                 show={result}
@@ -105,4 +209,4 @@ export function Register() {
             </Form>
         </Container>
     );
-}
+*/
